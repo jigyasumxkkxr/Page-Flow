feb5b0f741f80c8f2f51ace2e368612d6686019c
Jigyasu <133757043+jigyasumxkkxr@users.noreply.github.com>
2024-08-11 17:45:07 +0530

    height-corrected

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -18,7 +18,7 @@ export const Blogs = () => {
     }
     else {
         return (
-            <div className="absolute inset-0 h-fit max-w-screen bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+            <div className="absolute inset-0 max-h-fit min-h-screen max-w-screen bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
                 <Nav />
                 <div className="w-4/5 m-auto mb-10 p-4 rounded-t-xl h-fit mt-10 flex flex-col gap-3 backdrop-blur-xs shadow-md">
                     {blogs.map(blog => <BlogCard id={blog.id} title={blog.title} description = {blog.content} publishedDate="Posted on a day in 2024" author= {blog.author?.name ?? 'Anonymous'}  />)}
