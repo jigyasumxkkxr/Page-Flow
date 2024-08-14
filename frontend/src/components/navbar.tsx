f620a7992ca24053abe0f7742437e33025e3fe61
Jigyasu <133757043+jigyasumxkkxr@users.noreply.github.com>
2024-08-14 10:18:58 +0530

    loader-removed+responsive

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -14,7 +14,7 @@ export const NavBar = () => {
     return (
         <div className="h-14 flex justify-between mt-2">
             <Link to={"/"} >
-            <div className="h-full  mx-3 sm:mx-8 flex items-center gap-3">
+            <div className="h-full  mx-1 sm:mx-8 flex items-center gap-3">
                 <div className="w-2/5 sm:w-2/5 h-full">
                     <img src={logo} alt="" className="h-full" />
                 </div>
@@ -25,7 +25,7 @@ export const NavBar = () => {
             </div>
             </Link>
 
-            <div className="h-full flex mx-3 sm:mx-8 gap-3 sm:gap-6 items-center">
+            <div className="h-full flex mx-2 sm:mx-8 gap-3 sm:gap-6 items-center">
                 <div>
                     <ButtonDark label="Get Started" onClick={SignUpHandeler} />
                 </div>
