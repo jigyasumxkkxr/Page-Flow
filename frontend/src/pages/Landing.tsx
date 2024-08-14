035fd6bfbba3eddbdd345e49d0221dd4eb00d41d
Jigyasu <133757043+jigyasumxkkxr@users.noreply.github.com>
2024-08-14 10:07:06 +0530

    loader-removed

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,30 +1,13 @@
-import { useEffect, useState } from "react"
+
 import { Footer } from "../components/footer"
 import { HeroLanding } from "../components/landing-hero"
 import { LandingImage } from "../components/landing-img"
 import { LandingSubHero } from "../components/landing-subhero"
 import { NavBar } from "../components/navbar"
-import { SunspotLoaderComponent } from "../components/loader"
 
 
 export const Landing = () => {
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 1000);
-
-        return () => clearTimeout(timer);
-    }, [])
-    if(loading) {
-        return (
-            <div className="flex h-screen w-screen justify-center items-center">
-                <SunspotLoaderComponent />
-            </div>
-        )
-    }
-
-    else {
+    
         return (
             <div className="absolute inset-0 min-h-screen max-h-fit w-screen bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] flex flex-col justify-between">
                 <NavBar />
@@ -38,5 +21,5 @@ export const Landing = () => {
                 <Footer />
             </div>
         )
-    }
+    
 }
